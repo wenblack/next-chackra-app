07329e8ed84b221b7b69c78d1d0346f2add98ae8
Wender Barbosa <wenblack012@gmail.com>
2023-03-22 11:04:59 -0300

    Created theme and cookie to recieve and save color theme of APp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider >
       <Component {...pageProps} />
     </ChakraProvider>
   )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,16 @@
+import { ColorModeScript } from '@chakra-ui/react'
 import { Html, Head, Main, NextScript } from 'next/document'
+import { theme } from '../theme'
+
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head title='Star Wars WIKI' />
       <body>
         <Main />
         <NextScript />
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       </body>
     </Html>
   )
